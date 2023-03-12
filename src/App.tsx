46b76d7a6bdc05a05a3ba15b27addc759b9398d4
miyamonz <m5813k@gmail.com>
2023-03-12 16:36:19 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Heading } from "@chakra-ui/react";
 
-import { Demo as Demo1 } from "./demo/TaskApp";
-import { Demo as Demo2 } from "./demo_final/TaskApp";
+import { Demo, Demo as Demo1 } from "./demo1/TaskApp";
+import { Demo as Demo2 } from "./demo2/TaskApp";
+import { Router, usePath } from "./Router";
 
 export function App() {
+  const path = usePath();
   return (
     <div className="">
-      <Heading p={2}>simple task app</Heading>
-      <Demo1 />
-      <hr />
-      <Demo2 />
+      <Heading p={2}>
+        simple task app
+        <Router />
+      </Heading>
+      {path === "/demo1" && <Demo1 />}
+      {path === "/demo2" && <Demo2 />}
     </div>
   );
 }
--- src/Router.tsx
+++ src/Router.tsx
@@ -1,28 +1,29 @@
-import { Select, Stack } from "@chakra-ui/react";
+import { Box, Select, Stack } from "@chakra-ui/react";
 import { useAtom } from "jotai";
 import { atomWithLocation } from "jotai-location";
 
 const locationAtom = atomWithLocation();
+export function usePath() {
+  const [location] = useAtom(locationAtom);
+  return location.pathname;
+}
 export function Router() {
-  const [location, setLocation] = useAtom(locationAtom);
+  const [, setLocation] = useAtom(locationAtom);
   return (
-    <Stack direction="row" p={2}>
-      <span>location: </span>
-      <Select
-        display="inline"
-        size="xs"
-        w="40"
-        onChange={(e) => setLocation({ pathname: e.target.value })}
-      >
-        {[
-          { pathname: "/", label: "Home" },
-          { pathname: "/about", label: "About" },
-        ].map((item) => (
-          <option key={item.pathname} value={item.pathname}>
-            {item.label}
-          </option>
-        ))}
-      </Select>
-    </Stack>
+    <Select
+      display="inline-block"
+      size="md"
+      w={40}
+      onChange={(e) => setLocation({ pathname: e.target.value })}
+    >
+      {[
+        { pathname: "/demo1", label: "demo1" },
+        { pathname: "/demo2", label: "demo2" },
+      ].map((item) => (
+        <option key={item.pathname} value={item.pathname}>
+          {item.label}
+        </option>
+      ))}
+    </Select>
   );
 }
